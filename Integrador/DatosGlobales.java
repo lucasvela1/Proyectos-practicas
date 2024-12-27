3a6c4914b7d28af0b0ab344d7bc43ba526db10e9
Lucas Vela <87398590+lucasvela1@users.noreply.github.com>
2024-12-27 14:54:50 -0300

    Add files via upload

---
+++ Integrador/DatosGlobales.java
@@ -0,0 +1,9 @@
+
+import java.util.ArrayList;
+
+//Clase para guardar los datos usados en el programa.
+
+public class DatosGlobales{
+    public static ArrayList<PlanDeEstudio> listaPlanes= new ArrayList<>();
+    public static ArrayList<Carrera> listaCarreras=new ArrayList<>();
+}
--- Integrador/VentanaCarreras.java
+++ Integrador/VentanaCarreras.java
@@ -1,17 +1,61 @@
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class VentanaCarreras extends JFrame{
+
+    //Elementos de la ventana
+    private JComboBox<String> comboCarreras;
+    private static JButton botonAgregarCarrera = new JButton("Agregar carrera");
+    private static JButton botonAgregarPlan =new JButton("Agregar plan de estudios");
+    private JComboBox<String> comboPlanes;
+
+
     public VentanaCarreras(JFrame ventanaOpciones) {
+        //Configuración de la ventana
         this.setTitle("Gestión de Carreras");
         this.getContentPane().setBackground(Color.ORANGE);
-        this.setLayout(new FlowLayout());
-        this.add(new JLabel("Ventana de Gestión de Carreras"));
+        this.setLayout(new BorderLayout());
+        
+        //Configuración del combobox
+        comboCarreras= new JComboBox<>();
+        comboCarreras.setBounds(100, 100, 150, 30);
+        cargarCarrerasCombo();
+        this.getContentPane().add(comboCarreras, BorderLayout.NORTH);
+
+        //Panel con botones en el margen inferior, con fondo gris oscuro.
+        JPanel panelBotonesInferior= new JPanel();
+        panelBotonesInferior.setBackground(Color.DARK_GRAY);
+        panelBotonesInferior.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        this.getContentPane().add(panelBotonesInferior, BorderLayout.SOUTH);
+        botonAgregarCarrera.setBackground(Color.CYAN);
+        botonAgregarPlan.setBackground(Color.CYAN);
+        panelBotonesInferior.add(botonAgregarCarrera, BorderLayout.SOUTH);
+        panelBotonesInferior.add(botonAgregarPlan, BorderLayout.SOUTH);
+        
+        botonAgregarCarrera.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                agregarCarrera();
+            }
+        });
+
+
+        botonAgregarPlan.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                agregarPlan();
+            }
+        });
 
         // Agregar listener para el cierre de la ventana
         this.addWindowListener(new WindowAdapter() {
@@ -22,4 +66,21 @@ public class VentanaCarreras extends JFrame{
             }
         });
     }
+
+    public void agregarCarrera(){
+       VentanaAgregarCarrera ventanaAgregarCarrera = new VentanaAgregarCarrera(this);
+       ventanaAgregarCarrera.setVisible(true);
+       this.setVisible(false);
+    }
+
+    public void agregarPlan(){
+
+    }
+
+    private void cargarCarrerasCombo(){
+    comboPlanes.removeAllItems();
+    for (Carreras carrera: DatosGlobales.listaCarreras){
+        comboCarreras.addItem(carrera);
+    }
+   }
 }
