af423b26e4ac6dcdb20139cfc4b6fdf5537c9308
Lucas Vela <87398590+lucasvela1@users.noreply.github.com>
2025-01-07 14:45:44 -0300

    Delete Integrador directory

--- Integrador/DatosGlobales.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.ArrayList;
-
-//Clase para guardar los datos usados en el programa.
-
-public class DatosGlobales{
-    public static ArrayList<PlanDeEstudio> listaPlanes= new ArrayList<>();
-    public static ArrayList<Carrera> listaCarreras=new ArrayList<>();
-}
