3fe7b446a94888e83eea14c25aa5d6aace15daf6
Lucas Vela <87398590+lucasvela1@users.noreply.github.com>
2025-05-24 09:30:35 -0300

    Delete FaltanteAlCambio directory

--- FaltanteAlCambio/main.py
+++
@@ -1,5 +0,0 @@
-from src.ui.ventana_info import VentanaInfo
-
-if __name__ == "__main__":
-    app = VentanaInfo()
-    app.mainloop()
--- FaltanteAlCambio/src/tools/read_config.py
+++
@@ -1,7 +0,0 @@
-import json
-
-
-def read_config(key):
-    with open("config.json") as file:
-        data = json.load(file)
-    return data.get(key)
